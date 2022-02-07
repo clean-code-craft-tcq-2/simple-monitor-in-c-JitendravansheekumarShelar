ebe0280d46959582d69c5e8507c731474b754360
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-07 23:27:26 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
+#include <checker.h>
 
-struct data
-{
-    float testTemperature,testSoc,testChargeRate;
-};
+void printOnConsole(char msg[]) {
+    printf("%s out of range!\n",msg);
+}
 
 int isChargeRateOutOfRange(const struct data f_BatteryInformation)
 {
     if(f_BatteryInformation.testChargeRate > 0.8)
     {
-        printf("Charge Rate out of range!\n");
+        printOnConsole("Charge Rate out of range!\n");
         return 0;
     }
     return 1;
@@ -20,7 +20,7 @@ int isSocOutOfRange(const struct data f_BatteryInformation)
 {
      if(f_BatteryInformation.testSoc < 20 || f_BatteryInformation.testSoc > 80) 
      {
-        printf("State of Charge out of range!\n");
+        printOnConsole("State of Charge out of range!\n");
         return 0;
     }
     return isChargeRateOutOfRange(f_BatteryInformation); //return could be either 0 or 1
@@ -30,13 +30,13 @@ int isTemperatureOutOfRange(const struct data f_BatteryInformation)
 {
      if(f_BatteryInformation.testTemperature < 0 || f_BatteryInformation.testTemperature > 45) 
      {
-        printf("Temperature out of range!\n");
+        printOnConsole("Temperature out of range!\n");
         return 0;
     }
     return isSocOutOfRange(f_BatteryInformation); //return could be either 0 or 1
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
+int testbatteryIsOk(float temperature, float soc, float chargeRate) {
  struct data l_BatteryInformation;
  l_BatteryInformation.testTemperature = temperature;
  l_BatteryInformation.testSoc = soc;
@@ -45,8 +45,24 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
  return isTemperatureOutOfRange(l_BatteryInformation); //return could be either 0 or 1 
 }
 
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  if(temperature < 0 || temperature > 45) {
+    printf("Temperature out of range!\n");
+    return 0;
+  } else if(soc < 20 || soc > 80) {
+    printf("State of Charge out of range!\n");
+    return 0;
+  } else if(chargeRate > 0.8) {
+    printf("Charge Rate out of range!\n");
+    return 0;
+  }
+  return 1;
+}
+
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
-  assert(!batteryIsOk(25, 60, 0.9));  
+  #if (SW_BUILD == TEST_ENV)
+  assert(testbatteryIsOk(25, 70, 0.7));
+  assert(!testbatteryIsOk(50, 85, 0));
+  assert(!testbatteryIsOk(25, 60, 0.9)); 
+  #endif
 }
