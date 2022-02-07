127fa2dee20927477154aa2076f8a230c2f5f662
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-07 23:32:42 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -1,1 +1,15 @@
+#define TemperatureMinLimit 0
+#define TemperatureMaxLimit 45
+#define SOCMinLimit 20
+#define SOCMaxLimit 80
+#define ChargeRateMinLimit 0.0
+#define ChargeRateMaxLimit 0.8
 
+struct BMSdata
+{
+    float testTemperature,testSoc,testChargeRate;
+};
+
+void printOnConsole(char msg[]);
+int testbatteryIsOk(float temperature, float soc, float chargeRate);
+int isParameterOutOfRange(const struct data f_ParameterInformation);
