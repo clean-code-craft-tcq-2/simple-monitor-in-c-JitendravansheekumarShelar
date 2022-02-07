b84f3aa980ad5602d53b8faa438985fb70595dc5
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-08 01:24:21 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -5,11 +5,27 @@
 #define ChargeRateMinLimit 0.0
 #define ChargeRateMaxLimit 0.8
 
-struct BMSdata
-{
-    float testTemperature,testSoc,testChargeRate;
+#define TEST_FAILED 0
+#define TEST_PASSED 1
+
+#define MAX_ARRAY_CONTENT 4
+#define MAX_ARRAY_LENGTH 3
+
+typedef struct {
+  float parameterValue;
+  float minValue;
+  float maxValue;
+  char parameterMessage[20];
+} BMSParameterInfo;
+
+BMSParameterInfo BMSParameterData[][MAX_ARRAY_CONTENT] = {
+    {0,TemperatureMinLimit,TemperatureMaxLimit,"Temperature"},
+    {0,SOCMinLimit,SOCMaxLimit,"State of charge"},
+    {0,ChargeRateMinLimit,ChargeRateMaxLimit,"Charge Rate"}
 };
 
-void printOnConsole(char msg[]);
-int testbatteryIsOk(float temperature, float soc, float chargeRate);
-int isParameterOutOfRange(const struct data f_ParameterInformation);
+int testPassedCounter = 0;
+
+void printOnConsole(char inputMsg[] , int testResult , int testcaseNumber);
+int isParameterOutOfRange(const BMSParameterInfo f_BMSParameterData[][2]);
+void testbatteryIsOk(float * f_BMSData);
