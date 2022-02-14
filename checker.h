d694a43c7b0542f662f2f6e99a026b1dbcbf29a9
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-14 21:40:05 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -1,32 +1,17 @@
+//define parameters
+#define TEMPERATURE 0
+#define SOC 1
+#define CHARGE_RATE 2
+
+//define parameters limits
 #define TemperatureMinLimit 0
 #define TemperatureMaxLimit 45
 #define SOCMinLimit 20
 #define SOCMaxLimit 80
-#define ChargeRateMinLimit 0.0
 #define ChargeRateMaxLimit 0.8
 
-#define TEST_FAILED 0
-#define TEST_PASSED 1
-
-#define MAX_ARRAY_CONTENT 4
-#define MAX_ARRAY_LENGTH 3
-
-typedef struct {
-  float parameterValue;
-  float minValue;
-  float maxValue;
-  char parameterMessage[20];
-} BMSParameterInfo;
-
-BMSParameterInfo BMSParameterData[][MAX_ARRAY_CONTENT] = {
-    {0,TemperatureMinLimit,TemperatureMaxLimit,"Temperature"},
-    {0,SOCMinLimit,SOCMaxLimit,"State of charge"},
-    {0,ChargeRateMinLimit,ChargeRateMaxLimit,"Charge Rate"}
-};
-
-int testPassedCounter = 0;
-
-void printOnConsole(const char* inputMsg , int testResult , int testcaseNumber);
-int isParameterOutOfRange(const BMSParameterInfo f_BMSParameterData[][MAX_ARRAY_CONTENT]);
-void testbatteryIsOk(float * f_BMSData);
-int funcRangeCheck(const BMSParameterInfo f_BMSParameterData[][MAX_ARRAY_CONTENT],int indexValue);
+//declarations
+int isChargeRateOutOfRange(float * f_BMSData);
+int isSOCOutOfRange(float * f_BMSData);
+int isTemperatureOutOfRange(float * f_BMSData);
+int testBatteryIsOk(float * f_BMSData);
