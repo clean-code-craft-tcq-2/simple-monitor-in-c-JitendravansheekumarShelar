76d78033441e3ddf5ba8d0191553497659790b34
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-14 21:39:23 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <assert.h>
 #include "checker.h"
@@ -6,105 +7,63 @@
 #define TEST_ENV 1
 #define SW_BUILD TEST_ENV
 
-/*void printOnConsole(const char* inputMsg , int testResult , int testcaseNumber) {
-    if(testResult != TEST_FAILED)
-    {
-        printf("Testcase no %d passed\n",testcaseNumber);
-    }
-    else
-    {
-        printf("Testcase no %d failed : %s out of range!\n",testcaseNumber,inputMsg);
-    }
-}*/
 
-/* Use of recurssive function , but due to cyclomatic complexity it is commented */
-/*int isParameterOutOfRange(const BMSParameterInfo f_BMSParameterData[][MAX_ARRAY_CONTENT],int indexValue)
+int isChargeRateOutOfRange(float * f_BMSData)
 {
-    if(indexValue >= MAX_ARRAY_LENGTH )
+    if(f_BMSData[CHARGE_RATE] > ChargeRateMaxLimit) 
     {
-        printf("Testcase no %d passed\n",testPassedCounter);
-        return 1;
+        printf("Charge Rate out of range!\n");
+        return 0;
     }
-    
-    if((f_BMSParameterData[indexValue]->parameterValue < f_BMSParameterData[indexValue]->minValue) ||\
-       (f_BMSParameterData[indexValue]->parameterValue > f_BMSParameterData[indexValue]->maxValue)) 
-    {
-            printf("Testcase no %d failed : %s out of range!\n",testPassedCounter,(f_BMSParameterData[indexValue]->parameterMessage));
-            return 0;
-    }
-    
-    return isParameterOutOfRange(BMSParameterData,indexValue+1);
-}*/
-
-
-int funcRangeCheck(const BMSParameterInfo f_BMSParameterData[][MAX_ARRAY_CONTENT],int indexValue)
-{
-    
-    if((f_BMSParameterData[indexValue]->parameterValue < f_BMSParameterData[indexValue]->minValue) ||\
-       (f_BMSParameterData[indexValue]->parameterValue > f_BMSParameterData[indexValue]->maxValue))
-       {
-           return 0;
-       }
-    else
-    {
-        return 1;
-    }
-}
-
-int isParameterOutOfRange(const BMSParameterInfo f_BMSParameterData[][MAX_ARRAY_CONTENT])
-{
-    int result = 0;
-    for(int indexValue=0; indexValue < (MAX_ARRAY_CONTENT-1); indexValue++)
-    {
-        result = funcRangeCheck(BMSParameterData,indexValue);
-        if(!result)
-        {
-            printf("Testcase no %d failed : %s out of range!\n",testPassedCounter,(f_BMSParameterData[indexValue]->parameterMessage));
-            assert(result==0);
-            return 0;
-        }
-    }
-    printf("Testcase no %d passed\n",testPassedCounter);
-    assert(result==1);
     return 1;
 }
 
-void testbatteryIsOk(float * f_BMSData) {
-    int result =0;
-    
-    testPassedCounter+=1;
-    //Update Data which is passed as input
-    for (int i=0;i < (MAX_ARRAY_CONTENT-1) ;i++)
+int isSOCOutOfRange(float * f_BMSData)
+{
+    if(f_BMSData[SOC] < SOCMinLimit || f_BMSData[SOC] > SOCMaxLimit) 
     {
-        BMSParameterData[i]->parameterValue = f_BMSData[i];
+        printf("SOC out of range!\n");
+        return 0;
     }
-    
-    result = isParameterOutOfRange(BMSParameterData);    
+    return isChargeRateOutOfRange(&f_BMSData[0]);
 }
 
-//ORIGINAL FUNCTION
-/*int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
-}*/
+int isTemperatureOutOfRange(float * f_BMSData)
+{
+    if(f_BMSData[TEMPERATURE] < TemperatureMinLimit || f_BMSData[TEMPERATURE] > TemperatureMaxLimit) 
+    {
+        printf("Temperature out of range!\n");
+        return 0;
+    }
+    return isSOCOutOfRange(&f_BMSData[0]);
+}
+
+//TEST FUNCTION
+int testBatteryIsOk(float * f_BMSData) {
+   int testFlag = 0;
+   
+   testFlag = isTemperatureOutOfRange(&f_BMSData[0]);
+   
+   if (testFlag)
+   {
+       assert(testFlag == 1);
+   }
+   else
+   {
+       assert(testFlag == 0);
+   }
+   
+   
+}
+
 
 int main() {
   #if (SW_BUILD == TEST_ENV)
   float BMSData_A[] = {25,70,0.7};
   float BMSData_B[] = {50,85,0};
   float BMSData_C[] = {25,60,0.9};
-  testbatteryIsOk(BMSData_A);
-  testbatteryIsOk(BMSData_B);
-  testbatteryIsOk(BMSData_C); 
+  testBatteryIsOk(BMSData_A);
+  testBatteryIsOk(BMSData_B);
+  testBatteryIsOk(BMSData_C); 
   #endif
 }
-
