3bf9c0d99aeb8072330db85ffb24aeea5e286beb
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-15 21:58:05 +0530

    Update TemperatureRangeCheck.c

--- TemperatureRangeCheck.c
+++ TemperatureRangeCheck.c
@@ -3,37 +3,35 @@
 #include "TemperatureRangeCheck.h"
 #include "SOCRangeCheck.h"
 
-int temperatureWarningCheck(float * f_BMSData)
+
+int Temperature_RangeCheck(float * f_BMSData)
 {
+    #if (TEMPERATURE_WARNING_CHECK == 1)
     if(f_BMSData[TEMPERATURE] > TemperatureWarningMinLimit && f_BMSData[TEMPERATURE] < TemperatureMinLimit) 
     {
-        printf("Warning : Temperature out of range!\n");
+        printf("Warning : Drop in Temperature !\n");
+        return 1; //Since it is warning
+    }
+    
+    if (f_BMSData[TEMPERATURE] > TemperatureWarningMaxLimit && f_BMSData[TEMPERATURE] < TemperatureMaxLimit) 
+    {
+        printf("Warning : Rise in Temperature !\n");
+        return 1; //Since it is warning
+    }
+    #endif
+    
+    if(f_BMSData[TEMPERATURE] < TemperatureMinLimit || f_BMSData[TEMPERATURE] > TemperatureMaxLimit)
+    {
+        
+        printf("Error : Temperature out of range!\n");
         return 0;
     }
-    else if (f_BMSData[TEMPERATURE] > TemperatureWarningMaxLimit && f_BMSData[TEMPERATURE] < TemperatureMaxLimit) 
-    {
-        printf("Warning : Temperature out of range!\n");
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
+
+    return 1;
+
 }
 
 int isTemperatureOutOfRange(float * f_BMSData)
 {
-    int warningCheckFlag = 1;
-    
-    #if (TEMPERATURE_WARNING_CHECK == 1)
-    warningCheckFlag = temperatureWarningCheck(&f_BMSData[0]);
-    #endif
-    
-    if((f_BMSData[TEMPERATURE] < TemperatureMinLimit || f_BMSData[TEMPERATURE] > TemperatureMaxLimit) &&\
-        (warningCheckFlag == 1))
-    {
-        printf("Error : Temperature out of range!\n");
-        return 0;
-    }
-    return isSOCOutOfRange(&f_BMSData[0]);
+    return Temperature_RangeCheck(&f_BMSData[0]) ? isSOCOutOfRange(&f_BMSData[0]) : 0;
 }
