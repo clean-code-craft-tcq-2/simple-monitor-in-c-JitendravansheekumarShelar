3b747c161235a7cda1342e664027ca688e3b545c
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-15 21:57:31 +0530

    Update SOCRangeCheck.h

--- SOCRangeCheck.h
+++ SOCRangeCheck.h
@@ -1,8 +1,8 @@
 //define parameters limits
 #define SOCMinLimit 20
 #define SOCMaxLimit 80
-#define SOCWarningMinLimit 76
-#define SOCWarningMaxLimit 24
+#define SOCWarningMinLimit 24
+#define SOCWarningMaxLimit 76
 
 extern int isSOCOutOfRange(float * f_BMSData);
-static int SOCWarningCheck(float * f_BMSData);
+static int SOC_RangeCheck(float * f_BMSData);
