74b8899b19fcc3a5d4291946130c5243c12cfd53
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-15 21:56:33 +0530

    Update ChargeRateRangeCheck.h

--- ChargeRateRangeCheck.h
+++ ChargeRateRangeCheck.h
@@ -1,7 +1,7 @@
 //define parameters limits
 #define ChargeRateMaxLimit 0.8
-#define ChargeRateWarningMaxLimit 0.076
+#define ChargeRateWarningMaxLimit 0.76
 
 //declarations
 extern int isChargeRateOutOfRange(float * f_BMSData);
-static int ChargeRateWarningCheck(float * f_BMSData);
+static int ChargeRate_RangeCheck(float * f_BMSData);
