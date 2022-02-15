0c5b77a0b85f49a77cf5d6ca9c4742c676bdae01
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-15 21:57:03 +0530

    Update SOCRangeCheck.c

--- SOCRangeCheck.c
+++ SOCRangeCheck.c
@@ -3,37 +3,33 @@
 #include "SOCRangeCheck.h"
 #include "ChargeRateRangeCheck.h"
 
-int SOCWarningCheck(float * f_BMSData)
+int SOC_RangeCheck(float * f_BMSData)
 {
+    #if (SOC_WARNING_CHECK == 1)
     if(f_BMSData[SOC] > SOCWarningMinLimit && f_BMSData[SOC] < SOCMinLimit) 
     {
         printf("Warning : Approaching Discharge!\n");
-        return 0;
+        return 1; //Since it is warning
     }
-    else if (f_BMSData[SOC] > SOCWarningMaxLimit && f_BMSData[SOC] < SOCMaxLimit) 
+    
+    if (f_BMSData[SOC] > SOCWarningMaxLimit && f_BMSData[SOC] < SOCMaxLimit) 
     {
         printf("Warning : Approaching charge-peak!\n");
-        return 0;
+        return 1; //Since it is warning
     }
-    else
-    {
-        return 1;
-    }
-}
-
-int isSOCOutOfRange(float * f_BMSData)
-{
-    int warningCheckFlag = 1;
-    
-    #if (CHARGE_RATE_WARNING_CHECK == 1)
-    warningCheckFlag = SOCWarningCheck(&f_BMSData[0]);
     #endif
-    
-    if((f_BMSData[SOC] < SOCMinLimit || f_BMSData[SOC] > SOCMaxLimit) &&\
-        (warningCheckFlag == 1))
+
+    if(f_BMSData[SOC] < SOCMinLimit || f_BMSData[SOC] > SOCMaxLimit)
     {
         printf("Error : SOC out of range!\n");
         return 0;
     }
-    return isChargeRateOutOfRange(&f_BMSData[0]);
+    
+    return 1;
+
+}
+
+int isSOCOutOfRange(float * f_BMSData)
+{
+    return SOC_RangeCheck(&f_BMSData[0]) ? isChargeRateOutOfRange(&f_BMSData[0]) : 1;
 }
