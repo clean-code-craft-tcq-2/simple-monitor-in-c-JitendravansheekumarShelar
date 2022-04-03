44cfdd97d88daa2860965da54703d3dae573d5fb
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-03 13:22:38 +0530

    Update TemperatureRangeCheck.c

--- TemperatureRangeCheck.c
+++ TemperatureRangeCheck.c
@@ -3,32 +3,44 @@
 #include "TemperatureRangeCheck.h"
 #include "SOCRangeCheck.h"
 
-
-int Temperature_RangeCheck(float * f_BMSData)
+int isRiseInTemprature(float * f_BMSData)
 {
-    /*#if (TEMPERATURE_WARNING_CHECK == 1)
-    if(f_BMSData[TEMPERATURE] > TemperatureWarningMinLimit && f_BMSData[TEMPERATURE] < TemperatureMinLimit) 
-    {
-        printf("Warning : Drop in Temperature !\n");
-        return 1; //Since it is warning
-    }
-    
+    #if (TEMPERATURE_WARNING_CHECK == 1)
     if (f_BMSData[TEMPERATURE] > TemperatureWarningMaxLimit && f_BMSData[TEMPERATURE] < TemperatureMaxLimit) 
     {
         printf("Warning : Rise in Temperature !\n");
         return 1; //Since it is warning
     }
-    #endif*/
-    
-    if(f_BMSData[TEMPERATURE] < TemperatureMinLimit || f_BMSData[TEMPERATURE] > TemperatureMaxLimit)
+    #endif
+    return 0; //No Warning
+}
+
+int isDropInTemprature(float * f_BMSData)
+{
+    #if (TEMPERATURE_WARNING_CHECK == 1)
+    if(f_BMSData[TEMPERATURE] > TemperatureWarningMinLimit && f_BMSData[TEMPERATURE] < TemperatureMinLimit) 
     {
-        
+        printf("Warning : Drop in Temperature !\n");
+        return 1; //Since it is warning
+    }
+    #endif
+    return isRiseInTemprature(&f_BMSData[0]);
+}
+
+int Temperature_WarningRangeCheck(float * f_BMSData)
+{
+    return isDropInTemprature(&f_BMSData[0]);
+}
+
+int Temperature_RangeCheck(float * f_BMSData)
+{
+    
+    if(!Temperature_WarningRangeCheck(&f_BMSData[0]) && (f_BMSData[TEMPERATURE] < TemperatureMinLimit || f_BMSData[TEMPERATURE] > TemperatureMaxLimit))
+    {   
         printf("Error : Temperature out of range!\n");
         return 0;
     }
-
     return 1;
-
 }
 
 int isTemperatureOutOfRange(float * f_BMSData)
