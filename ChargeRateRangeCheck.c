94ccb05204b8a8d50f5695dd5dd9d0164cef82c7
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-03 13:36:18 +0530

    Update ChargeRateRangeCheck.c

--- ChargeRateRangeCheck.c
+++ ChargeRateRangeCheck.c
@@ -2,17 +2,25 @@
 #include "checker.h"
 #include "ChargeRateRangeCheck.h"
 
-int isChargeRateOutOfRange(float * f_BMSData)
+int isChargeRateApproachingDischarge(float * f_BMSData)
 {
-    /*#if (CHARGE_RATE_WARNING_CHECK == 1)
+
     if(f_BMSData[CHARGE_RATE] > ChargeRateWarningMaxLimit && f_BMSData[CHARGE_RATE] < ChargeRateMaxLimit) 
     {
         printf("Warning : Approaching Discharge!\n");
         return 1; //Since it is warning
     }
-    #endif*/
+    return 0;
+}
 
-    if(f_BMSData[CHARGE_RATE] > ChargeRateMaxLimit)
+int ChargeRate_WarningRangeCheck(float * f_BMSData)
+{
+    return isApproachingDischarge(&f_BMSData[0]);
+}
+
+int isChargeRateOutOfRange(float * f_BMSData)
+{
+    if(!ChargeRate_WarningRangeCheck(&f_BMSData[0]) && (f_BMSData[CHARGE_RATE] > ChargeRateMaxLimit))
     {
         printf("Error : Charge Rate out of range!\n");
         return 0;
